bb41a02fc88cb7955a1a9d30ba3b0a4abcedeaf9
sugar191 <pawaburo@gmail.com>
2025-05-31 14:15:00 +0900

    ゲーム差追加、menu外クリックで閉じる、検索をメニューに配置

--- home/views.py
+++ home/views.py
@@ -5,18 +5,32 @@ from teams.models import TeamSeason, League
 from games.models import Game
 
 
+def league_rank(league_id):
+    league = League.objects.get(id=league_id)
+    teams = TeamSeason.objects.filter(league=league_id).order_by(
+        "league__sort_order", "sort_order"
+    )
+
+    top_team = teams.first()
+
+    # 各チームにゲーム差を付与
+    for team in teams:
+        if team.win is not None and team.lose is not None:
+            team.game_behind = (
+                (top_team.win - team.win) + (team.lose - top_team.lose)
+            ) / 2
+        else:
+            team.game_behind = None
+
+    return league, teams
+
+
 # Create your views here.
 # 選手一覧を表示するビュー
 def top_page(request):
     today = date.today()
-    ce_league = League.objects.get(id=1)
-    pa_league = League.objects.get(id=2)
-    ce_teams = TeamSeason.objects.filter(league=1).order_by(
-        "league__sort_order", "sort_order"
-    )
-    pa_teams = TeamSeason.objects.filter(league=2).order_by(
-        "league__sort_order", "sort_order"
-    )
+    ce_league, ce_teams = league_rank(1)
+    pa_league, pa_teams = league_rank(2)
     players = PlayerLatestSummary.objects.filter(
         player_birthday__month=today.month, player_birthday__day=today.day
     )
